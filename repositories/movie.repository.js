0eaf63ba3e49338f2cb117668389b1ccc00b213f
dabeenkim <ekqls6812@naver.com>
2023-03-29 15:18:21 +0900

    [fix]프론트요청사항으로 수정

--- repositories/movie.repository.js
+++ repositories/movie.repository.js
@@ -136,7 +136,7 @@ class MovieRepository extends Content {
       })
       .map((p) => p.dataValues.codename);
     return {
-      [findOnesMovie.name]: findOnesMovie,
+      Video: findOnesMovie,
       actor: personCodes,
     };
   };
